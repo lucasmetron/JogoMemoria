f45590c05dbbf935942ddf4ba2e55a99af8656d9
lucasmetron <lucasmetron@gmail.com>
2021-03-29 14:01:21 -0300

    gerador de cartas automatico pelo JS

--- script/script.js
+++ script/script.js
@@ -1,7 +1,45 @@
-function vira(){
-    let carta = document.getElementsByClassName("card")[0]
-    let status = document.getElementsByName("status")
-    console.log(carta.children[0])
-    console.log(status)
-    carta.setAttribute("class", "card flip")
+const FRONT = "card_front";
+const BACK = "card_back";
+
+let techs = [
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react"
+];
+
+createCardsFromTechs(techs)
+
+function createCardsFromTechs(techs){
+    let cards = [];
+
+    for (let tech of techs){
+        cards.push(createPairFromTech(tech))
+    }
+
+    console.log(cards.map(pair=> pair));
+}
+
+function createPairFromTech(tech){
+    return[{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false
+    },
+    {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false
+    }
+]
+}
+
+function createIdWithTech (tech){
+    return tech + parseInt(Math.random() *1000);
 }
